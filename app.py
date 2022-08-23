20f4c4d05af589a03040ffbcc30dad0b1cb393d8
Alisteph <kmk.since2018.4@gmail.com>
2022-08-23 18:39:33 +0900

    add notes

--- app.py
+++ app.py
@@ -1,22 +1,28 @@
 import streamlit as st
 import chord_gen
+import display_txts as dtx
 
 
 st.title('AutoChordProgressGenerater')
 
 
-init_chords = st.text_input("input")
+init_chords = st.text_input("initial chords")
 num_chords = st.text_input("number of chords")
 exec_generate = st.button('exec')
 
+
 if exec_generate:
-    output_chord = chord_gen.generate_chord(init_chords.split())
-    if len(output_chord) > int(num_chords):
-        output_chord = output_chord[:int(num_chords)]
+    if len(init_chords.split()) == 0:
+        st.markdown(dtx.no_ini_chord, unsafe_allow_html=True)
+    elif len(num_chords.split()) == 0 or len(num_chords.split()) > 1:
+        st.markdown(dtx.not1num, unsafe_allow_html=True)
+    else:
+        output_chord = chord_gen.generate_chord(init_chords.split())
+        if len(output_chord) > int(num_chords):
+            output_chord = output_chord[:int(num_chords)]
 
-    output = st.text(' '.join(output_chord))
+        st.code(' '.join(output_chord))
 
-    st.write('MIDI download function is in progress...')
     # button_downloado = st.download_button(
     #     'download onchord',
     #     open('tmp/on_chord.mid', 'br'),
@@ -27,3 +33,5 @@ if exec_generate:
     #     open('tmp/chord.mid', 'br'),
     #     'chord.mid'
     # )
+
+st.markdown(dtx.description1)
---
+++ display_txts.py
@@ -0,0 +1,23 @@
+description1 = \
+    """
+    NOTE
+    - Key is supported for __Cmaj only__ (will be able to change the key in the future)
+    - 'initial chords' requires more than 0 codes (ex. 'F G Am7 Esus4')
+    - 'number of chords' requires integer < 50
+    - You can get a __different chord progression each time__ you execute it
+    - MIDI download function is in progress...    
+    """
+
+no_ini_chord = \
+    """
+    <span style="color: red; ">
+    Please enter the 'initial chords', which requires more than 0 codes (ex. 'F G Am7 Esus4')
+    </span>
+    """
+
+not1num = \
+    """
+    <span style="color: red; ">
+    Please enter the 'number of chords', which requires one integer (ex. '32')
+    </span>
+    """    
